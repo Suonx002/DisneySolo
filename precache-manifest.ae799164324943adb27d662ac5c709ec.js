2a4cd4b5a85347eb8c10664beab3b7c85ecc5c49
Vuthy <suonx002@gmail.com>
2019-12-28 20:00:06 -0600

    Updates

---
+++ precache-manifest.ae799164324943adb27d662ac5c709ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c7279151f9b9521d58912c2c52adfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9b41a13e553dd8df97e",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "979bb5750bbfa0f3263a",
+    "url": "/static/css/main.0a12c298.chunk.css"
+  },
+  {
+    "revision": "e9b41a13e553dd8df97e",
+    "url": "/static/js/2.e01fa682.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.e01fa682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "979bb5750bbfa0f3263a",
+    "url": "/static/js/main.3c1b8013.chunk.js"
+  },
+  {
+    "revision": "020a340421f4787bbdd1",
+    "url": "/static/js/runtime-main.86c4999c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
