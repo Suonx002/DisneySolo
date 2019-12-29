11659ec92ad529156c773b5e29bfe7bfb41c1103
Vuthy <suonx002@gmail.com>
2019-12-28 18:55:18 -0600

    little fix to video card

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Navbar.scss';
 
@@ -8,10 +9,35 @@ const Navbar = () => {
       <div className='navbar-logo'>Disney Solo</div>
       <div className='navbar-content'>
         <div className='navbar-pages'>
-          <div className='navbar-item'>
+          <Link to='/' className='navbar-item'>
+            <i className='fas fa-home' />{' '}
+            <span className='underline'>Home </span>
+          </Link>
+          <Link to='/' className='navbar-item'>
+            <i className='fas fa-search' />{' '}
+            <span className='underline'>Search </span>
+          </Link>
+          <Link to='/' className='navbar-item'>
+            <i className='fas fa-plus' />{' '}
+            <span className='underline'>Watchlist </span>
+          </Link>
+          <Link to='/' className='navbar-item'>
+            <i className='fas fa-star' />{' '}
+            <span className='underline'>Originals </span>
+          </Link>
+          <Link to='/' className='navbar-item'>
+            <i className='fas fa-compact-disc' />{' '}
+            <span className='underline'>Movies </span>
+          </Link>
+          <Link to='/' className='navbar-item'>
+            <i className='fas fa-video' />{' '}
+            <span className='underline'>Series </span>
+          </Link>
+          {/* <div className='navbar-item'>
+
             <i className='fas fa-home' /> Home
-          </div>
-          <div className='navbar-item'>
+          </div> */}
+          {/* <div className='navbar-item'>
             <i className='fas fa-search' /> Search
           </div>
           <div className='navbar-item'>
@@ -25,7 +51,7 @@ const Navbar = () => {
           </div>
           <div className='navbar-item'>
             <i className='fas fa-video' /> Series
-          </div>
+          </div> */}
         </div>
         <div className='navbar-account'>Profile</div>
       </div>
