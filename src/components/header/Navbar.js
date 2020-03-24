2700464ac3f74380793576d909915b358f59b928
Vuthy <suonx002@gmail.com>
2020-03-24 01:25:21 -0500

    add logo and fix style issue with header on mobile

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import DisneySolo from '../../img/DisneySolo.png';
 import './Navbar.scss';
 
 const Navbar = () => {
@@ -8,7 +9,11 @@ const Navbar = () => {
     <div className='navbar-header'>
       <div className='navbar-logo'>
         <Link to='/' className='navbar-item'>
-          Disney Solo
+          <img
+            src={DisneySolo}
+            alt='logo'
+            style={{ height: '100px', width: '100px', overflow: 'hidden' }}
+          />
         </Link>
       </div>
       <div className='navbar-content'>
@@ -21,43 +26,7 @@ const Navbar = () => {
             <i className='fas fa-search' />{' '}
             <span className='underline'>Search </span>
           </Link>
-          {/* <Link to='/' className='navbar-item'>
-            <i className='fas fa-plus' />{' '}
-            <span className='underline'>Watchlist </span>
-          </Link>
-          <Link to='/' className='navbar-item'>
-            <i className='fas fa-star' />{' '}
-            <span className='underline'>Originals </span>
-          </Link>
-          <Link to='/' className='navbar-item'>
-            <i className='fas fa-compact-disc' />{' '}
-            <span className='underline'>Movies </span>
-          </Link>
-          <Link to='/' className='navbar-item'>
-            <i className='fas fa-video' />{' '}
-            <span className='underline'>Series </span>
-          </Link> */}
-          {/* <div className='navbar-item'>
-
-            <i className='fas fa-home' /> Home
-          </div> */}
-          {/* <div className='navbar-item'>
-            <i className='fas fa-search' /> Search
-          </div>
-          <div className='navbar-item'>
-            <i className='fas fa-plus' /> WatchList
-          </div>
-          <div className='navbar-item'>
-            <i className='fas fa-star' /> Originals
-          </div>
-          <div className='navbar-item'>
-            <i className='fas fa-compact-disc' /> Movies
-          </div>
-          <div className='navbar-item'>
-            <i className='fas fa-video' /> Series
-          </div> */}
         </div>
-        {/* <div className='navbar-account'>Profile</div> */}
       </div>
     </div>
   );
